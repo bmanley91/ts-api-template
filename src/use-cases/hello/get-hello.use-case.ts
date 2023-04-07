991d9d934ce47ed568ef2a579d3b2372fdeaf1cc
Brian Manley <brian.manley91@gmail.com>
2023-04-07 18:34:08 -0400

    Fixed log

--- src/use-cases/hello/get-hello.use-case.ts
+++ src/use-cases/hello/get-hello.use-case.ts
@@ -4,7 +4,7 @@ import { HelloRepository } from '../../domain/repositories/hello.repository';
 export const getHelloUsecase = async (id: string, helloRepo: HelloRepository): Promise<Hello> => {
     const hello = await helloRepo.findById(id);
 
-    console.log(`Found Hello: ${hello}`);
+    console.log(`Found Hello: ${JSON.stringify(hello)}`);
 
     return hello;
 };
