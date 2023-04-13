ddfd54db2c0b7b856d9264bcd41d20b47f29019b
Brian Manley <brian.manley@wework.com>
2023-04-13 07:39:33 -0400

    json log format

--- src/infrastructure/util/logger.ts
+++ src/infrastructure/util/logger.ts
@@ -1,5 +1,6 @@
 import { createLogger, format, transports } from 'winston';
 import { getCorrelationId } from '../../api/middleware/logger.middleware';
+import { v4 as uuid } from 'uuid';
 
 export const logInfo = (message: string): void => {
     logWithContext('info', message);
@@ -9,26 +10,22 @@ export const logWarn = (message: string): void => {
     logWithContext('warn', message);
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const logError = (message: any): void => {
     logWithContext('error', message);
 };
 
 
-const { combine, timestamp, printf } = format;
-
-const customFormat = printf(({ timestamp, level, message, correlationId }) => {
-    return `${timestamp} [${correlationId}] ${level}: ${message}`;
-});
+const { combine, timestamp, prettyPrint } = format;
 
 const logger = createLogger({
-    // TODO: JSON format
-    format: combine(timestamp(), customFormat),
+    format: combine(timestamp(), prettyPrint()),
     transports: [new transports.Console()],
 });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const logWithContext = (level: 'info' | 'error' | 'warn', message: any) => {
-    const correlationId = getCorrelationId() || 'N/A';
+    const correlationId = getCorrelationId() || uuid();
     logger.log({ level, message, correlationId });
 };
 
