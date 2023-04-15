b8a29f15c260c14376c3e4bff00260ac28a99b90
Brian Manley <brian.manley91@gmail.com>
2023-04-15 19:55:02 -0400

    Added test around correlation id generation case

--- src/api/middleware/logger.middleware.test.ts
+++ src/api/middleware/logger.middleware.test.ts
@@ -3,7 +3,7 @@ import httpmocks from 'node-mocks-http';
 import { v4 as uuid } from 'uuid';
 
 describe ('Logger Middleware', () => {
-    it ('adds a correlation id to the request', () => {
+    it ('uses the X-Correlation-ID header as a correlation id', () => {
         const correlationId = uuid();
         const request = httpmocks.createRequest({
             headers: { 'X-Correlation-ID': correlationId }
@@ -18,5 +18,19 @@ describe ('Logger Middleware', () => {
 
         expect(next).toBeCalled();
         expect(actualCorrelationId).toBe(correlationId);
+    });
+
+    it ('generates a correlation id if none is provided', () => {
+        const request = httpmocks.createRequest();
+        const response = httpmocks.createResponse();
+        let actualCorrelationId: string | undefined;
+        const next = jest.fn(() => {
+            actualCorrelationId = getCorrelationId();
+        });
+
+        correlationMiddleware(request, response, next);
+
+        expect(next).toBeCalled();
+        expect(actualCorrelationId).toBeDefined();
     });
 });
